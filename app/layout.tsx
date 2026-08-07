cbb9a2954fe97c070af93d92341512550d48f428
Lady-Vanillaice <herzblutmadl3@gmail.com>
2026-08-07 10:08:13 +0200

    Load crimson red theme globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./alpha.css";
 import "./experience-layer.css";
+import "./red-theme.css";
 import CalendarNavigation from "./calendar-navigation";
 import ExperienceLayer from "./experience-layer";
 import PublicDirectoryNav from "./public-directory-nav";
